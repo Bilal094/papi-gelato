3f0c36cc7e7b5c1b378f0b7d05da3ccfbde8c42a
Bilal094 <99066318@mydavinci.nl>
2021-11-09 14:57:20 +0100

    Eerste 3 stappen afgemaakt, while loop gefixt, hashtags erin gezet

--- papi-gelato.py
+++ papi-gelato.py
@@ -6,6 +6,7 @@ from time import sleep
 
 Herhalen = True
 
+
 # User-Defined functions ---
 
 def showIntro():
@@ -24,22 +25,46 @@ def showErrorBakje():
 showIntro()
 pause()
 
+
 while Herhalen == True:
     Bolletjes = int(input('Hoeveel bolletjes wilt u? '))
+    
+    # Als de klant tussen de 1 of 3 bolletjes wilt
     if Bolletjes >= 1 and Bolletjes <= 3:
-        Herhalen = False
         AantalBolletjes = Bolletjes
         Bakje = input('Wilt u deze '+ str(Bolletjes) +' bolletje(s) in A) een hoorntje of B) een bakje? ')
         if Bakje.upper() == 'A':
-            print('test')
+            Doorgaan = input('Hier is uw hoorntje met '+ str(Bolletjes) +' bolletje(s). Wilt u nog meer bestellen? (Y/N) ').upper()
+            if Doorgaan == 'Y':
+                Herhalen = True
+            elif Doorgaan == 'N':
+                print('Bedankt en tot ziens!')
+                Herhalen = False
+            else:
+                showError()
         elif Bakje.upper() == 'B':
-            print('test2')
+            Doorgaan = input('Hier is uw bakje met '+ str(Bolletjes) +' bolletje(s). Wilt u nog meer bestellen? (Y/N) ').upper()
+            if Doorgaan == 'Y':
+                Herhalen = True
+            elif Doorgaan == 'N':
+                Herhalen = False
+                print('Bedankt en tot ziens!')
+            else:
+                showError()
+        else:
+            showError()
+    
+    # Als de klant tussen de 4 of 8 bolletjes wilt
     elif Bolletjes >= 4 and Bolletjes <= 8:
         AantalBolletjes = Bolletjes
         print('Dan krijgt u van mij een bakje met '+ str(Bolletjes) +' bolletjes.')
         Herhalen = False
+    
+    # Als de klant meer dan 8 bolletjes wilt, dan krijgt de klant een foutmelding te zien
     elif Bolletjes > 8:
         showErrorBakje()
+        Herhalen = True
+    # De klant kan niet voor 0 bolletjes kiezen, hij laat dan deze foutmelding zien
     elif Bolletjes == 0:
         showError()
-
+        Herhalen = True
