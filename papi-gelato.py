be964089c70bfe16e4c74da9e90109b6d533e16a
Bilal094 <99066318@mydavinci.nl>
2021-11-10 12:07:28 +0100

    Smaak features toegevoegd

--- papi-gelato.py
+++ papi-gelato.py
@@ -5,7 +5,8 @@ from time import sleep
 # Variabelen --- 
 
 Herhalen = True
-
+Bolsmaak = 1
+Smaken = ["A", "C", "M", "V", "a", "c", "m", "v" "Aardbei", "Chocolade", "Munt", "Vanille", "aardbei", "chocolade", "munt", "vanille"]
 
 # User-Defined functions ---
 
@@ -21,24 +22,34 @@ def showError():
 def showErrorBakje():
     print('Sorry, zulke grote bakken hebben we niet.')
 
+
 # Code start ---
 showIntro()
 pause()
 
 
 while Herhalen == True:
-    Bolletjes = int(input('Hoeveel bolletjes wilt u? '))
+    Bolletjes = input('Hoeveel bolletjes wilt u? ')
     
     # Als de klant tussen de 1 of 3 bolletjes wilt
-    if Bolletjes >= 1 and Bolletjes <= 3:
-        AantalBolletjes = Bolletjes
+    if Bolletjes.isdigit() >= 1 and Bolletjes.isdigit() <= 3:
+
+        # Hier wordt de klant gevraagd om de smaken voor zijn bolletjes
+        while Bolsmaak <= int(Bolletjes):
+            Smaak = input('Welke smaak wilt u voor bolletje nummer '+ str(Bolsmaak) +'? A) Aardbei, C) Chocolade, M) Munt of V) Vanille? ')
+            if Smaak in Smaken:
+                Bolsmaak += 1
+            else:
+                showError()
+        
+        # Hier wordt de klant gevraagd of hij een bakje of een hoorntje wilt en daarna wordt de code afgesloten of, als de klant het wilt, opnieuw 'gerund'
         Bakje = input('Wilt u deze '+ str(Bolletjes) +' bolletje(s) in A) een hoorntje of B) een bakje? ')
         if Bakje.upper() == 'A':
             Doorgaan = input('Hier is uw hoorntje met '+ str(Bolletjes) +' bolletje(s). Wilt u nog meer bestellen? (Y/N) ').upper()
             if Doorgaan == 'Y':
                 Herhalen = True
             elif Doorgaan == 'N':
-                print('Bedankt en tot ziens!')
+                print('Bedankt en tot ziens!') 
                 Herhalen = False
             else:
                 showError()
@@ -51,20 +62,26 @@ while Herhalen == True:
                 print('Bedankt en tot ziens!')
             else:
                 showError()
+                Herhalen = True
         else:
             showError()
-    
+        
     # Als de klant tussen de 4 of 8 bolletjes wilt
-    elif Bolletjes >= 4 and Bolletjes <= 8:
-        AantalBolletjes = Bolletjes
+    elif Bolletjes.isdigit() >= 4 and Bolletjes.isdigit() <= 8:
         print('Dan krijgt u van mij een bakje met '+ str(Bolletjes) +' bolletjes.')
+        while Bolsmaak <= int(Bolletjes):
+            Smaak = input('Welke smaak wilt u voor bolletje nummer '+ str(Bolsmaak) +'? A) Aardbei, C) Chocolade, M) Munt of V) Vanille? ')
+            if Smaak in Smaken:
+                Bolsmaak += 1
         Herhalen = False
     
     # Als de klant meer dan 8 bolletjes wilt, dan krijgt de klant een foutmelding te zien
-    elif Bolletjes > 8:
+    elif Bolletjes.isdigit() > 8:
         showErrorBakje()
         Herhalen = True
     # De klant kan niet voor 0 bolletjes kiezen, hij laat dan deze foutmelding zien
-    elif Bolletjes == 0:
+    elif Bolletjes.isdigit() == 0:
         showError()
         Herhalen = True
+    else:
+        showError()
